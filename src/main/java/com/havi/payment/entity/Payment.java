dbc94abd59271deb145a204c10d483f09843bf63
siriwardhanasmnp <siriwardhanasmnp.19@uom.lk>
2022-11-06 08:38:33 +0530

    Entities

--- src/main/java/com/havi/payment/entity/Payment.java
+++ src/main/java/com/havi/payment/entity/Payment.java
@@ -1,4 +1,20 @@
 package com.havi.payment.entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
